2fe1428f7fc3d278a502e0490840f86fba8e4fe9
SimoneUni <144710127+SimoneUni@users.noreply.github.com>
2023-09-29 15:28:02 +0200

    Add files via upload

---
+++ fatturazione_bnb/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'fatture_appartamenti',
+    'version': '1.0',
+    'author': 'Simone',
+    'description': 'fatturazione appartamenti yaaa ',
+    'depends': ['base','account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml'
+    ],
+    'application': True
+
+}
---
+++ fatturazione_bnb/models/fatt_main.py
@@ -0,0 +1,4 @@
+from odoo import models, fields
+
+class fatturebnb(models.Model):
+    _inherit = "account.move"
